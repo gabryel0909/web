92a6003f65ca1bd5c16b8f64bdab02fcd3ef3401
gabryel0909 <gabryelsantiago83@gmail.com>
2024-10-21 11:18:15 -0300

    Update urls.py

--- web/crud/urls.py
+++ web/crud/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('inicio',views.inicio),
-    path('p/', views.post_list, name='post-list'),
+    path('', views.post_list, name='post-list'),
     path('post-create', views.post_create, name='post-create'),
     path('post-detail/<int:id>/', views.post_detail, name='post-detail'),
     path('post-delete/<int:id>/', views.post_delete, name='post-delete'),
